97bc4d5f2bf2de501035b942635e779f81543dcb
darshan <darsha019@gmail.com>
2024-06-16 23:52:14 +0530

    minor fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/blog/",
   plugins: [react()],
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
 });
